24942db4d8a720f066cf36e7e80e01fe036ad60e
LBlend <lblend@protonmail.com>
2020-12-11 23:21:07 +0100

    added norwegian prediction translation

--- backend/src/classifier.py
+++ backend/src/classifier.py
@@ -14,16 +14,25 @@ def predict(text):
     text_features = feature_extractor.bigrams_stopwords(text_tokens)
     prediction = classifier.prob_classify(text_features)
 
+    norwegian = {
+        'man': 'mann',
+        'woman': 'kvinne'
+    }
+
     result['text'] = text
-    result['prediction'] = prediction.max()
+    result['prediction'] = {}
+
     result['likelyhood'] = {}
     result['likelyhood']['raw'] = {}
     result['likelyhood']['simple'] = {}
 
+    result['prediction']['norwegian'] = norwegian[prediction.max()]
+    result['prediction']['english'] = prediction.max()
+
     result['likelyhood']['raw']['man'] = prediction.prob('man')
     result['likelyhood']['raw']['woman'] = prediction.prob('woman')
 
-    result['likelyhood']['simple']['man'] = f'{prediction.prob("man") * 100:.2f}%'
-    result['likelyhood']['simple']['woman'] = f'{prediction.prob("woman") * 100:.2f}%'
+    result['likelyhood']['simple']['man'] = f'{prediction.prob("man") * 100:.0f}%'
+    result['likelyhood']['simple']['woman'] = f'{prediction.prob("woman") * 100:.0f}%'
 
     return result
