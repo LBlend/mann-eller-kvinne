9bc2ad5748e80dd6ce78d28d08431110cd1f048d
LBlend <lblend@protonmail.com>
2021-01-06 23:52:22 +0100

    added image alt text

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -9,7 +9,7 @@ function About() {
       </nav>
         <article>
             <h1>Om siden</h1>
-            <img src='https://raw.githubusercontent.com/LBlend/mann-eller-kvinne/main/.static/mann-eller-kvinne.png?token=AF55TQTQ2EPX6JP33MO4NLK73QQZG'></img>
+            <img src='https://raw.githubusercontent.com/LBlend/mann-eller-kvinne/main/.static/mann-eller-kvinne.png?token=AF55TQTQ2EPX6JP33MO4NLK73QQZG' alt="Logo"></img>
             <p>Nettsida gjetter om du er mann eller kvinne ved bruk av enkel maskinlæring. Maskinlæringsmodellen er trent på over 3000 anmeldelser fra norsk media og har som formål å finne ut av hva som skiller kvinner og menn skriftlig.</p>
             <p>Konseptet er inspirert av debatten som oppstod i Berteheussen-saken, hvor det skal ha blitt diskutert hvorvidt det er en mann som kan ha skrevet trusselbrevene som er omtalt. Dette på grunn av at ordet "tisse" ble brukt fremfor ordet "pisse". Du kan lese mer om saken <a href='https://www.nrk.no/kultur/uenige-om-bruken-av-ordet-_tisse_-1.15206839'>her</a>.</p>
             <p>Om du mener at denne kan forbedres, kan du gjerne bidra. Kan du react eller har kunnskap om maskinlæring så er det bare å åpne opp din favoritteditor og hive seg på!</p>
