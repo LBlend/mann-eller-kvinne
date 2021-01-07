903d5a9b90dd1730fe6e6d6eb8987a5fa5be83ce
marksverdhei <marksverdhei@hotmail.com>
2021-01-07 12:38:59 +0100

    Replace nltk_install.py with single-line command

--- backend/build_backend.sh
+++ backend/build_backend.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
+python3 -m pip install -r requirements.txt
+
 if [ ! -d 'corpus' ]; then
     mkdir corpus
     git clone https://github.com/ltgoslo/norec_gender.git corpus
-    python3 -m pip install -r requirements.txt
     python3 ./src/scripts/organize_corpus.py
     python3 ./src/scripts/clean_data.py
 fi
 
-python3 nltk_install.py
+python3 -c "import nltk; nltk.download('stopwords'); nltk.download('punkt')"
 
 if [ ! -e 'bin/bayes_model.pkl' ]; then
     python3 ./src/scripts/train_bayes.py
