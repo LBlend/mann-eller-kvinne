f3abfb86c02d07c9586a082c1c2fed81359f53ee
LBlend <lblend@protonmail.com>
2021-01-12 02:00:25 +0100

    Fixed standardJS linting errors

--- frontend/src/components/API.js
+++ frontend/src/components/API.js
@@ -1,32 +1,32 @@
-const api_base_url = "https://genderapi.lblend.moe"
-// const api_base_url = "http://127.0.0.1:5000"
+const apiBaseUrl = 'https://genderapi.lblend.moe'
+// const apiBaseUrl = 'http://127.0.0.1:5000'
 
 export const predict = async (text) => {
   if (text.length > 5000) {
-    document.body.style.backgroundColor = '#171520';
+    document.body.style.backgroundColor = '#171520'
     return (
       {
         probability: {
-          'M': 0,
-          'F': 0
-        },
+          M: 0,
+          F: 0
+        }
       }
     )
   }
 
-  let data = {
+  const data = {
     text: text,
     clf: 'bayes'
     // clf: 'rnn'
   }
   console.log(data)
-  const rawResponse = await fetch(`${api_base_url}/mann-eller-kvinne`, {
+  const rawResponse = await fetch(`${apiBaseUrl}/mann-eller-kvinne`, {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json',
+      'Content-Type': 'application/json'
     },
     body: JSON.stringify(data)
-  });
-  const response = await rawResponse.json();
-  return response;
+  })
+  const response = await rawResponse.json()
+  return response
 }
--- frontend/src/components/Prediction.js
+++ frontend/src/components/Prediction.js
@@ -1,53 +1,52 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
-import { predict } from '../components/API';
+import { predict } from '../components/API'
 
-function toPercent(n) {
+function toPercent (n) {
   return `${Math.round(n * 100)}%`
 }
 
-function Prediction() {
-  const [text, setText] = useState('');
-  const [prediction, setPrediction] = useState('');
-  const [man, setMan] = useState('');
-  const [woman, setWoman] = useState('');
+function Prediction () {
+  const [text, setText] = useState('')
+  const [prediction, setPrediction] = useState('')
+  const [man, setMan] = useState('')
+  const [woman, setWoman] = useState('')
 
   useEffect(() => {
     predict(text).then((result) => {
-      setPrediction(result.probability['M'] > result.probability['F'] ? 'mann' : 'kvinne');
-      setMan(toPercent(result.probability['M']));
-      setWoman(toPercent(result.probability['F']));
-    });
-  }, [text]);
+      setPrediction(result.probability.M > result.probability.F ? 'mann' : 'kvinne')
+      setMan(toPercent(result.probability.M))
+      setWoman(toPercent(result.probability.F))
+    })
+  }, [text])
 
   if (text === '') {
-      document.body.style.backgroundColor = '#171520';
-      return (
-        <div className='prediction'>
-            <textarea onChange={(e) => setText(e.target.value)} />
-            <h3>🤖 Maskinen gjetter 🤖</h3>
-            <p>ingenting... <i>foreløpig</i> 👀</p>
-        </div>
-      )
+    document.body.style.backgroundColor = '#171520'
+    return (
+      <div className='prediction'>
+        <textarea onChange={(e) => setText(e.target.value)} />
+        <h3>🤖 Maskinen gjetter 🤖</h3>
+        <p>ingenting... <i>foreløpig</i> 👀</p>
+      </div>
+    )
   }
 
   if (prediction === 'mann') {
-      document.body.style.backgroundColor = '#2C77FF';
-  }
-  else if (prediction === 'kvinne') {
-    document.body.style.backgroundColor = 'lightcoral';
+    document.body.style.backgroundColor = '#2C77FF'
+  } else if (prediction === 'kvinne') {
+    document.body.style.backgroundColor = 'lightcoral'
   }
 
   return (
     <div className='prediction'>
-        <textarea onChange={(e) => setText(e.target.value)} />
-        <h3>🤖 Maskinen gjetter 🤖</h3>
-        <p>Du er sannsynligvis en <b>{prediction}</b></p>
-        <br />
-        <p>Sannsynlighet for mann: {man}</p>
-        <p>Sannsynlighet for kvinne: {woman}</p>
+      <textarea onChange={(e) => setText(e.target.value)} />
+      <h3>🤖 Maskinen gjetter 🤖</h3>
+      <p>Du er sannsynligvis en <b>{prediction}</b></p>
+      <br />
+      <p>Sannsynlighet for mann: {man}</p>
+      <p>Sannsynlighet for kvinne: {woman}</p>
     </div>
-  );
+  )
 }
 
-export default Prediction;
+export default Prediction
