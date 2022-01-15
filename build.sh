ac3f55e50573a15af780d367f1d061a504be6eb6
LBlend <lblend@protonmail.com>
2022-01-15 18:28:44 +0100

    add progress messages

--- build.sh
+++ build.sh
@@ -1,20 +1,25 @@
 #!/bin/sh
 
+echo "Installing Python dependencies..."
 python3 -m pip install -r requirements.txt -U
 
 if [ ! -d 'corpus' ]; then
+    echo "Downloading corpus..."
     mkdir corpus
     git clone https://github.com/ltgoslo/norec_gender.git corpus
     python3 ./src/scripts/organize_corpus.py
     python3 ./src/scripts/clean_data.py
 fi
 
+echo "Downloading NLTK stopwords and punctuation packages..."
 python3 -c "import nltk; nltk.download('stopwords'); nltk.download('punkt')"
 
 if [ ! -e 'bin/bayes_model.pkl' ]; then
+    echo "Training Bayes model..."
     python3 ./src/scripts/train_bayes.py
 fi
 
 if [ ! -d 'bin/rnn' ]; then
+    echo "Training RNN model..."
     python3 ./src/scripts/train_rnn.py
 fi
