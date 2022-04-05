5763632740b0ab596f2ba95398d35b7c3e2a714b
LBlend <lblend@protonmail.com>
2022-04-05 15:37:40 +0200

    use semantic variable names in API interface

--- src/classifier.py
+++ src/classifier.py
@@ -18,10 +18,10 @@ rnn_model = keras.models.load_model("src/bin/rnn")
 
 def predict_bayes(text: str) -> dict[str, float]:
     features = vectorizer.transform([text])
-    probs = bayes_model.predict_proba(features)
+    probs = bayes_model.predict_proba(features)[0]
     return {
-        "F": probs[0],
-        "M": probs[1],
+        "male": probs[1],
+        "female": probs[0],
     }
 
 
@@ -29,12 +29,12 @@ def predict_rnn(text: str) -> dict[str, float]:
     pred_arr = rnn_model.predict(np.array([text]))
     pred = float(pred_arr[0, 0])
     # F = 0, M = 1, P(!A) == 1 - P(A)
-    return {"M": pred, "F": 1 - pred}
+    return {"male": pred, "female": 1 - pred}
 
 
 pred_funcs = {"bayes": predict_bayes, "rnn": predict_rnn}
 
 
-def predict(text: str, clf: str = "bayes") -> dict[str, str | dict[str, float]]:
-    predict_proba = pred_funcs[clf]
-    return {"text": text, "clf": clf, "probability": predict_proba(text)}
+def predict(text: str, classifier: str = "bayes") -> dict[str, str | dict[str, float]]:
+    predict_proba = pred_funcs[classifier]
+    return {"text": text, "classifier": classifier, "probability": predict_proba(text)}
